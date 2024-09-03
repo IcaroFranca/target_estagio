54d9c6387d68a24384632f802ffd5a5574e66e5f
GodIcarus <icarofranca12345@gmail.com>
2024-09-03 20:07:23 -0300

    questao_um

---
+++ questao_um.java
@@ -0,0 +1,17 @@
+
+public class questao_um {
+
+	public static void main(String[] args) {
+		
+		int indice = 13, soma = 0, k = 0;
+		
+		while (k < indice) {
+			k += 1;
+			soma += k;
+		}
+		
+		System.out.println(soma); // O resultado da 91 
+		
+	}
+
+}
